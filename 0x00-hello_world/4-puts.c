29b349b06eef521e4b85783857d8a21ba84b6904
datkiller <mr84112@gmail.com>
2023-02-16 14:53:51 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * Function a program that print
  */
-int main() {
-	  puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+	  puts( "Programming is like building a multilingual puzzle" );
 	    return 0;
 }
