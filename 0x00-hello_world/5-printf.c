cd12c1101852907ade6d2c2a798dd7eb016f76a7
datkiller <mr84112@gmail.com>
2023-02-16 15:29:13 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - print function
- * Return: 0 easy 
+ * Ri:::::eturn: 0 easy 
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
