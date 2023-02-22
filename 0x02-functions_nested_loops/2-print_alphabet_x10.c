8f21ffff30803c067cf2b3abbd7235d259d02cc4
datkiller <mr84112@gmail.com>
2023-02-21 18:05:28 -0800

    n

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int z = 0;
+
+	while (z <= 9)
+	{
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	z++;
+	}
+}
