cc9fded630f0b256dae733fe3349b202c0c77c79
datkiller <mr84112@gmail.com>
2023-02-22 18:11:58 -0800

    C is fun

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the value
+ *
+ * @i: input int
+ *
+ * Return: main value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
