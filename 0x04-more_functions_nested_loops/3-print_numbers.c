6b1294e16103a60f91f9ea48781a27c3458a596b
datkiller <mr84112@gmail.com>
2023-02-23 16:06:49 -0800

    DO HARD THINGS

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,13 @@
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9
  *
- * @x: int number
- *
  * Return: none
  */
 
 void print_numbers(void)
 {
 	int x;
-	
+
 	for (x = 0; x <= 9; x++)
 	{
 		_putchar(x + '0');
