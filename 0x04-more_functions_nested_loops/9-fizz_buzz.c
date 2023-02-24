073ac2717441aac0e3c15a7d0999854ec7669c64
datkiller <mr84112@gmail.com>
2023-02-23 17:48:57 -0800

    DO HARD THINGS

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - that prints the numbers from 1 to 100
+* But for multiples of three print -Fizz- instead of the number and for the multiples of five print -Buzz-
+*
+* Return: 0 sucess
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("fizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
