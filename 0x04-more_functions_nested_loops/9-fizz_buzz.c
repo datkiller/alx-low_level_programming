e9e1b844c9624b82961ce9d9c7d75ddf6335bf67
datkiller <mr84112@gmail.com>
2023-02-23 18:02:14 -0800

    DO HARD THINGS

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-* main - that prints the numbers from 1 to 100
-* But for multiples of three print -Fizz- instead of the number and for the multiples of five print -Buzz-
-*
-* Return: 0 sucess
-*/
+ * main - that prints the numbers from 1 to 100
+ * But for multiples of three print -Fizz- instead of the number and for the multiples of five print
+ *
+ * Return: 0 sucess
+ */
 
 int main(void)
 {
